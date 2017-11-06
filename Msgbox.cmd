318f42874fd44646a1737ec0f5fd28fc65da8bf8
@justyy <oh@steakovercooked.com>
2017-11-06 22:28:07 +0000

    Update Msgbox.cmd

--- Msgbox.cmd
+++ Msgbox.cmd
@@ -1,6 +1,6 @@
 @echo off
 rem Example Msgbox "Hello World!"
-rem https://helloacm.com
+rem https://helloacm.com/a-simple-msgbox-command-line-utility-in-windows-batch/
 
 set TMPFILE="%~dp0%RANDOM%.vbs"
 if not [%1]==[] (	
