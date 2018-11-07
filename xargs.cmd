067a8f365f143aa47e709502ce66a5a4142fa70a
@justyy <dr.zhihua.lai@gmail.com>
2018-11-07 14:03:36 +0000

    support -a read from file
    xargs -a input.txt echo

--- xargs.cmd
+++ xargs.cmd
@@ -1,10 +1,28 @@
 @echo off
 :: example:   git branch | grep -v "develop" | xargs git branch -D
+:: example    xargs -a input.txt echo
 :: https://helloacm.com/simple-xargs-batch-implementation-for-windows/
-setlocal
+setlocal enabledelayedexpansion
 
+set args=
+set file='more'
+
+:: read from file
+if "%1" == "-a" (
+    if "%2" == "" (
+        echo Correct Usage: %0 -a Input.txt command
+        goto end
+    )
+    set file=%2
+    shift
+    shift
+    goto start
+)
+
+:: read from stdin
 set args=%1
 shift
+
 :start
     if [%1] == [] goto start1
     set args=%args% %1
@@ -12,6 +30,8 @@ shift
     goto start
 
 :start1
-    for /F "tokens=*" %%a in ('more') do (
+    for /F "tokens=*" %%a in (!file!) do (
         %args% %%a
     )
+
+:end
