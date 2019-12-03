8813d125ebf6ca2cb73d77496c990e4131ec002d
@justyy <dr.zhihua.lai@gmail.com>
2019-12-03 16:26:02 +0000

    Update popd.bat

--- popd.bat
+++ popd.bat
@@ -6,13 +6,22 @@ if "%PUSHD_COUNT%" == "" (
 set /a PUSHD_COUNT=%PUSHD_COUNT-1
 set TMP=PUSHD%PUSHD_COUNT%
 
+set TMPFILE=%RANDOM%.tmp
+
 setlocal enabledelayedexpansion
 if "!%TMP%!" == "" (
     echo popd: directory stack empty
     goto :eof
 )
-set TMP="!%TMP%!"
-cd /d %TMP%
+set TMPDIR=!%TMP%!
+rem endlocal will erase the chdir and therefore 
+rem saving the file in a temp file
+echo %TMPDIR% > %TMPFILE%
 endlocal
+
+rem reading the directory from a file
+set /p TMP=<%TMPFILE%
+del /f %TMPFILE%
+chdir /d "%TMP%"
 
 set PUSHD%PUSHD_COUNT%=
