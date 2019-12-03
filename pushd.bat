e84c6c50acbbd2c22d6c073d118f12f64243597f
@justyy <dr.zhihua.lai@gmail.com>
2019-12-03 17:24:37 +0000

    Update pushd.bat

--- pushd.bat
+++ pushd.bat
@@ -1,20 +1,20 @@
 @echo off
-if "%1" == "" (
+if [%1] == [] (
     echo pushd: no other directory
     goto :eof
 )
 set TMP=%cd%
-if "%TMP%" == "" (
+if [%TMP%] == [] (
     for /f %%f in ('cd') do (
         set TMP=%%f
     )
 )
 cd /d "%1" 2> nul
-if "%errorlevel%" == "1" (
+if [%errorlevel%] == [1] (
     echo pushd: %1: No such file or directory
     goto :eof
 )
-if "%PUSHD_COUNT%" == "" (
+if [%PUSHD_COUNT%] == [] (
     set PUSHD_COUNT=0
 )
 set PUSHD%PUSHD_COUNT%=%TMP%
